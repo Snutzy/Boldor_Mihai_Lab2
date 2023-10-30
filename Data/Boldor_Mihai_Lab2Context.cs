1088022f99e9e7aaf99a6bf2704651a24c53e06c
Mihai <Mihai@LAPTOP-GU2M0CAT>
2023-10-30 19:26:55 +0200

    Add project files.

---
+++ Data/Boldor_Mihai_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Boldor_Mihai_Lab2.Models;
+
+namespace Boldor_Mihai_Lab2.Data
+{
+    public class Boldor_Mihai_Lab2Context : DbContext
+    {
+        public Boldor_Mihai_Lab2Context (DbContextOptions<Boldor_Mihai_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Boldor_Mihai_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Boldor_Mihai_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
